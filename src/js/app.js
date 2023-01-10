8bf965b3f424aaca50bf522fce698cff95d0b13b
GerganaNoneva <gergananoneva@gmail.com>
2023-01-10 19:17:48 +0200

    Add fire emoji

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let elementsPrice = document.getElementsByClassName('price');
+
+  for(let i = 0;i<elementsPrice.length;i++) {
+    console.log(elementsPrice[i].getAttribute('class'));
+    if(elementsPrice[i].getAttribute('class').includes('hot')==true) {
+      elementsPrice[i].innerHTML+=' &#128293;';
+    }
+  }
 });
