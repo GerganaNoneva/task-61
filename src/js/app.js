276c7032c173f2c9f109b2b85177dc2bb19bd8ba
GerganaNoneva <gergananoneva@gmail.com>
2023-01-10 19:45:12 +0200

    Make with querySellector

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,10 @@
-import "../scss/app.scss";
+//import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  let elementsPrice = document.getElementsByClassName('price');
 
-  for(let i = 0;i<elementsPrice.length;i++) {
-    console.log(elementsPrice[i].getAttribute('class'));
-    if(elementsPrice[i].getAttribute('class').includes('hot')==true) {
-      elementsPrice[i].innerHTML+=' &#128293;';
-    }
+  let elementsPrice=document.querySelectorAll('.price'&&'.hot');
+  for(let i=0; i<elementsPrice.length; i++) {
+      elementsPrice[0].innerHTML+=' &#128293;';
   }
 });
